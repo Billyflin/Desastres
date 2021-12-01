a1caff6d93e261beb78cf777ae25872c38243aae
Billy <billymartinezc@gmail.com>
2021-12-01 10:34:51 -0300

    total

---
+++ src/main/java/main/Launcher.java
@@ -0,0 +1,24 @@
+package main;
+
+import vista.MainGui;
+
+import javax.swing.*;
+import java.io.FileNotFoundException;
+
+
+
+public class Launcher {
+    public static void main(String[] args) throws FileNotFoundException {
+       runCreator();
+    }
+    public static void runCreator(){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainGui GUI =  new MainGui();
+                GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                GUI.setVisible(true);
+            }
+        });
+    }
+
+}
